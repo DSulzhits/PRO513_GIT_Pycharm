710112cfc60f7ad9bdb356419c1faaee9d312f83
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2026-02-14 14:09:27 +0300

    Rename func multiply >> product. Add correct multiply func.

--- math_funcs.py
+++ math_funcs.py
@@ -6,11 +6,25 @@ def subtract(a, b):
     return a - b
 
 
-def multiply(a, b):
+def product(a, b):
     return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError('На ноль делить нельзя!')
+    return a / b
+
+
+def multiply(a, b):
+    if not isinstance(b, int):
+        raise ValueError('Степень должна быть целым числом!')
+    return a ** b
 
 
 if __name__ == '__main__':
     print(add(3, 5))
     print(subtract(5, 3))
-    print(multiply(3, 5))
+    print(product(3, 5))
+    print(divide(15, 3))
+    print(multiply(2, 4))
